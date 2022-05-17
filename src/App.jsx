74becc27a6e82f029b1c137b9259251735ed8ca4
malikkanika9 <97448286+malikkanika9@users.noreply.github.com>
2022-05-17 19:50:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import RestaurantList from './components/RestaurantList'
+
+function App() {
+  return (
+    <div className="App">
+
+        <RestaurantList />
+    </div>
+  );
+}
+
+export default App;
+
